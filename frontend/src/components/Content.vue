6df57f87d1e1c9580910eb3384f8eb7df1a2e8dc
Olivier Pierre <olivierpierre85@gmail.com>
2021-08-02 17:35:02 +0200

    small changes for test

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -4,20 +4,24 @@
       <section class="nes-container is-dark">
           <section class="message-list">
               <section class="message -left">
-              <img src="@/assets/yukiopixel.png"/>
-              <div class="nes-balloon from-left is-dark">
-                  <p>Plainly there is no way back. Like it or not, we are stuck with science. We had better make the best of it. When we finally come to terms with it and fully recognize its beauty and its power, we will find, in spiritual as well as in practical matters, that we have made a bargain strongly in our favour.</p>
-              </div>
-              </section>
+                <div class="message-img">
+                    <img src="@/assets/yukiopixel.png"/>
+                </div>
+                <!-- Balloon -->
+                <div class="nes-balloon from-left is-dark">
+                    <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+                  <p>It's disheartening to discover government corruption and incompetence, for example; but it is better not to know about it? Whose interest does ignorance serve? If we humans bear, say, hereditary propensities toward the hatred of strangers, isn't self-knowledge the only antidote? If we long to believe that the stars rise and set for us, that we are the reason there is a Universe, does science do us a disservice in deflating our conceits?</p>
+              
+                </div>
+            </section>
       
-              <section class="message -right">
-              <!-- Balloon -->
+              <!-- <section class="message -right">
               <div class="nes-balloon from-right is-dark">
                   <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
                   <p>It's disheartening to discover government corruption and incompetence, for example; but it is better not to know about it? Whose interest does ignorance serve? If we humans bear, say, hereditary propensities toward the hatred of strangers, isn't self-knowledge the only antidote? If we long to believe that the stars rise and set for us, that we are the reason there is a Universe, does science do us a disservice in deflating our conceits?</p>
-              </div>
+              </div> 
 
-              </section>
+              </section> -->
           </section>
       </section>
 
@@ -48,4 +52,14 @@ export default {
     border-top: 0px;
 }
 
+.message-img {
+    position: relative;
+    display: inline-block;
+    width: 256px;
+    height: 256px;
+}
+
+.message {
+    display: flex;
+}
 </style>
