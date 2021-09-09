9181a8e0e043ebe59c82e8d9f935874f84ffbf99
Olivier Pierre <olivierpierre85@gmail.com>
2021-09-09 14:42:30 +0200

    decent look for login vue

--- public_html/src/components/Login.vue
+++ public_html/src/components/Login.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
-  <div class="container text-dark">
-    <div class="row justify-content-md-center">
-      <div class="col-md-5 p-3 login justify-content-md-center">
-        <h1 class="h3 mb-3 font-weight-normal text-center">Please sign in</h1>
+  <div class="nes-container">
+    <div class="nes-container login">
+      <h1>Please sign in</h1>
 
-        <p v-if="incorrectAuth">Incorrect username or password entered - please try again</p>
-        <form v-on:submit.prevent="login">
-          <div class="form-group">
-            <input type="text" name="username" id="user" v-model="username" class="form-control" placeholder="Username">
-          </div>
-          <div class="form-group">
-            <input type="password" name="password" id="pass" v-model="password" class="form-control" placeholder="Password">
-          </div>
-          <button type="submit" class="btn btn-lg btn-primary btn-block">Login</button>
-        </form>
-        
-      </div>
+      <p v-if="incorrectAuth">Incorrect username or password entered - please try again</p>
+      <form v-on:submit.prevent="login">
+        <div class="nes-field">
+          <input type="text" name="username" id="user" v-model="username" class="nes-input" placeholder="Username">
+        </div>
+        <div class="nes-field">
+          <input type="password" name="password" id="pass" v-model="password" class="nes-input" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-lg btn-primary btn-block">Login</button>
+      </form>
     </div>
   </div>
   </div>
@@ -54,11 +51,15 @@
 body { 
   background-color:#f4f4f4;
 }
-  .login{
-    background-color:#fff;
-    margin-top:10%;
-  }
-  input {
+.login{
+  background-color:#fff;
+  margin-top:100px;
+  text-align: center;
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+}
+input {
     padding: 25px 10px;
 }
 </style>
