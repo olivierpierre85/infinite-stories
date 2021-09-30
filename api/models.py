fecf7dff56a22c3f0649127c4256e84c95542d86
Olivier Pierre <olivierpierre85@gmail.com>
2021-09-30 17:45:06 +0200

    add files for prod

--- api/models.py
+++ api/models.py
@@ -12,16 +12,6 @@ class Storyline(MPTTModel):
     class Meta:
         ordering = ['created']
 
-# class Storyline(models.Model):
-#     created     = models.DateTimeField(auto_now_add=True)
-#     content     = models.TextField()
-#     is_author   = models.BooleanField(default=True)
-#     is_ending   = models.BooleanField(default=False)
-#     parent      = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
-
-#     class Meta:
-#         ordering = ['created']
-
 class Story(models.Model):
     name = models.CharField(max_length = 200, blank = False, default="Time Transmitter")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
